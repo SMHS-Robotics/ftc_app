85f551fdb60d9f3bacab022ab861330f060fa2a5
CouriersRyan <zhangryan09@gmail.com>
2019-01-30 14:56:04 -0800

    Added things from Kyle's Branch

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IMUTestCalibration.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IMUTestCalibration.java
@@ -1,40 +1,9 @@
-/* Copyright (c) 2017 FIRST. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted (subject to the limitations in the disclaimer below) provided that
- * the following conditions are met:
- *
- * Redistributions of source code must retain the above copyright notice, this list
- * of conditions and the following disclaimer.
- *
- * Redistributions in binary form must reproduce the above copyright notice, this
- * list of conditions and the following disclaimer in the documentation and/or
- * other materials provided with the distribution.
- *
- * Neither the name of FIRST nor the names of its contributors may be used to endorse or
- * promote products derived from this software without specific prior written permission.
- *
- * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
- * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.hardware.bosch.BNO055IMU;
-import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ReadWriteFile;
 
 import org.firstinspires.ftc.robotcore.external.Func;
 import org.firstinspires.ftc.robotcore.external.navigation.Acceleration;
@@ -44,29 +13,26 @@ import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
 import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 import org.firstinspires.ftc.robotcore.external.navigation.Position;
 import org.firstinspires.ftc.robotcore.external.navigation.Velocity;
+import org.firstinspires.ftc.robotcore.internal.system.AppUtil;
 
+import java.io.File;
 import java.util.Locale;
 
-@Autonomous(name = "Sensor: BNO055 IMU", group = "Sensor")
-// Comment this out to add to the opmode list
-public class IMUTest extends LinearOpMode
+@TeleOp(name = "Sensor: BNO055 IMU Calibration", group = "Sensor")
+public class IMUTestCalibration extends LinearOpMode
 {
     //----------------------------------------------------------------------------------------------
     // State
     //----------------------------------------------------------------------------------------------
 
-    // The IMU sensor object
+    // Our sensors, motors, and other devices go here, along with other long term state
     BNO055IMU imu;
-
-    //Left and Right wheel DC Motors
-    private DcMotor leftDrive = null;
-    private DcMotor rightDrive = null;
 
     // State used for updating telemetry
     Orientation angles;
     Acceleration accel;
-    Position location;
     Velocity speed;
+    Position location;
 
     //----------------------------------------------------------------------------------------------
     // Main logic
@@ -76,50 +42,66 @@ public class IMUTest extends LinearOpMode
     public void runOpMode()
     {
 
-        // Set up the parameters with which we will use our IMU. Note that integration
-        // algorithm here just reports accelerations to the logcat log; it doesn't actually
-        // provide positional information.
+        telemetry.log().setCapacity(12);
+        telemetry.log().add("");
+        telemetry.log().add("Please refer to the calibration instructions");
+        telemetry.log().add("contained in the Adafruit IMU calibration");
+        telemetry.log().add("sample opmode.");
+        telemetry.log().add("");
+        telemetry.log().add("When sufficient calibration has been reached,");
+        telemetry.log().add("press the 'A' button to write the current");
+        telemetry.log().add("calibration data to a file.");
+        telemetry.log().add("");
+
+        // We are expecting the IMU to be attached to an I2C port on a Core Device Interface Module and named "imu".
         BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
-        parameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
-        parameters.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
-        parameters.calibrationDataFile = "IMUTestCalibration.json"; // see the calibration sample opmode
         parameters.loggingEnabled = true;
         parameters.loggingTag = "IMU";
-        parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
-
-        //Retrieve and initialize left and right motors
-
-        // Retrieve and initialize the IMU. We expect the IMU to be attached to an I2C port
-        // on a Core Device Interface Module, configured to be a sensor of type "AdaFruit IMU",
-        // and named "imu".
         imu = hardwareMap.get(BNO055IMU.class, "imu");
         imu.initialize(parameters);
 
-        // Set up our telemetry dashboard
         composeTelemetry();
+        telemetry.log().add("Waiting for start...");
 
         // Wait until we're told to go
-        waitForStart();
-
-        // Start the logging of measured acceleration
-        imu.startAccelerationIntegration(new Position(), new Velocity(), 1000);
-
-        // Loop and update the dashboard
-        while (opModeIsActive())
+        while (!isStarted())
         {
             telemetry.update();
+            idle();
+        }
 
-//            final double initAngle = angles.firstAngle;
-//            while (Math.abs(angles.firstAngle - initAngle) < 90)
-//            {
-//                leftDrive.setPower(1);
-//            }
+        telemetry.log().add("...started...");
+
+        while (opModeIsActive())
+        {
+
+            if (gamepad1.a)
+            {
+
+                // Get the calibration data
+                BNO055IMU.CalibrationData calibrationData = imu.readCalibrationData();
+
+                // Save the calibration data to a file. You can choose whatever file
+                // name you wish here, but you'll want to indicate the same file name
+                // when you initialize the IMU in an opmode in which it is used. If you
+                // have more than one IMU on your robot, you'll of course want to use
+                // different configuration file names for each.
+                String filename = "IMUTestCalibration.json";
+                File file = AppUtil.getInstance().getSettingsFile(filename);
+                ReadWriteFile.writeFile(file, calibrationData.serialize());
+                telemetry.log().add("saved to '%s'", filename);
+
+                // Wait for the button to be released
+                while (gamepad1.a)
+                {
+                    telemetry.update();
+                    idle();
+                }
+            }
+
+            telemetry.update();
         }
     }
-
-    //----------------------------------------------------------------------------------------------
-    // Telemetry Configuration
-    //----------------------------------------------------------------------------------------------
 
     void composeTelemetry()
     {
@@ -188,5 +170,4 @@ public class IMUTest extends LinearOpMode
     {
         return String.format(Locale.getDefault(), "%.1f", AngleUnit.DEGREES.normalize(degrees));
     }
-
 }
