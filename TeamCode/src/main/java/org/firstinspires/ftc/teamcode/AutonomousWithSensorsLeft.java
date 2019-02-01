16e9baa961fd316a69ad8d247ef69f9efbebaa33
CouriersRyan <zhangryan09@gmail.com>
2019-01-31 16:37:51 -0800

    added Angles

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousWithSensorsLeft.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousWithSensorsLeft.java
@@ -1,14 +1,28 @@
 package org.firstinspires.ftc.teamcode;
 
+import android.print.PageRange;
+
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.Range;
 
 @Autonomous(name = "LEFT SIDE", group = "asdofijefj")
 public class AutonomousWithSensorsLeft extends LinearOpMode
 {
+    HardwarePushbot robot = new HardwarePushbot();
     @Override
     public void runOpMode() throws InterruptedException
     {
+        waitForStart();
 
+        while(opModeIsActive())
+        {
+
+        }
+    }
+
+    private void rotate(int degrees, double power)
+    {
+        power = Range.clip(power, 0, 1);
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwarePushbot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwarePushbot.java
@@ -1,41 +1,19 @@
-/* Copyright (c) 2017 FIRST. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted (subject to the limitations in the disclaimer below) provided that
- * the following conditions are met:
- *
- * Redistributions of source code must retain the above copyright notice, this list
- * of conditions and the following disclaimer.
- *
- * Redistributions in binary form must reproduce the above copyright notice, this
- * list of conditions and the following disclaimer in the documentation and/or
- * other materials provided with the distribution.
- *
- * Neither the name of FIRST nor the names of its contributors may be used to endorse or
- * promote products derived from this software without specific prior written permission.
- *
- * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
- * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
 import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DistanceSensor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.navigation.Acceleration;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 
 /**
  * This is NOT an opmode.
@@ -71,6 +49,12 @@ public class HardwarePushbot
     HardwareMap hwMap = null;
     private ElapsedTime period = new ElapsedTime();
 
+    //Parameters for REV Expansion Hub
+    Orientation angles;
+    Orientation lastAngles = new Orientation();
+    double globalAngle = 0;
+    Acceleration accel;
+
     /* Constructor */
     public HardwarePushbot()
     {
@@ -89,6 +73,17 @@ public class HardwarePushbot
         arm = hwMap.get(DcMotor.class, "arm");
         leftDrive.setDirection(DcMotor.Direction.FORWARD); // Set to REVERSE if using AndyMark motors
         rightDrive.setDirection(DcMotor.Direction.REVERSE);// Set to FORWARD if using AndyMark motors
+
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.calibrationDataFile = "IMUTestCalibration.json"; // see the calibration sample opmode
+        parameters.loggingEnabled = true;
+        parameters.loggingTag = "IMU";
+        parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
+
+        imu = hwMap.get(BNO055IMU.class, "imu");
+        imu.initialize(parameters);
 
         // Set all motors to zero power
         leftDrive.setPower(0);
@@ -110,5 +105,25 @@ public class HardwarePushbot
         //TODO: Initialize sensors
         distanceSensor = hwMap.get(DistanceSensor.class, "sensor_color_distance");
         colorSensor = hwMap.get(ColorSensor.class, "sensor_color_distance");
+
+        //Set Up Angle
+        resetAngle();
+
     }
+
+    public double getAngle()
+    {
+        angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+
+        return 0;
+
+    }
+
+    private void resetAngle()
+    {
+        lastAngles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+
+        globalAngle = 0;
+    }
+
 }
