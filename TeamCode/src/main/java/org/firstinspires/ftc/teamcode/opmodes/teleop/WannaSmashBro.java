cab8e431f46138fb23117688e69d199e85c16963
Klye Zhang <zkyle1337@gmail.com>
2019-03-07 17:05:20 -0800

    Reorganized everything into folder, added some pid stuff in "ryan kun uwu", copy pasted vuforia example

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/WannaSmashBro.java
@@ -0,0 +1,41 @@
+package org.firstinspires.ftc.teamcode.opmodes.teleop;
+
+import android.util.Log;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.Range;
+
+import org.firstinspires.ftc.teamcode.hardware.HardwareDummybot;
+
+@TeleOp(name = "CummyBoy", group = "Pushbot")
+
+public class WannaSmashBro extends LinearOpMode
+{
+
+    HardwareDummybot robot = new HardwareDummybot();
+
+    @Override
+    public void runOpMode()
+    {
+        double left;
+        double right;
+
+        robot.init(hardwareMap);
+
+        waitForStart();
+
+        while(opModeIsActive())
+        {
+            left = Range.clip(gamepad1.left_stick_y, -1, 1);
+            right = Range.clip(gamepad1.right_stick_y, -1, 1);
+
+            robot.leftDrive.setPower(left);
+            robot.rightDrive.setPower(right);
+
+            sleep(25);
+        }
+
+    }
+
+}
